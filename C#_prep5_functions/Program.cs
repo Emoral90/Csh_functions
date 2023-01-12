b39d7f189de7e83cb9a6d1beb8c03c562666f68b
gasolina90 <moralerick90@gmail.com>
2023-01-12 11:17:32 -0700

    modified file

--- C#_prep5_functions/Program.cs
+++ C#_prep5_functions/Program.cs
@@ -6,11 +6,31 @@
 
 // First 3 functions will be static
 // Or able to be called without creating a new instance of an object
-static void WelcomeMessage(){
-    // Void means there will be no parameters or a return type
-    Console.WriteLine("Welcome to hell!");
-}
 
-static void PromptUsername(){
-    
+
+
+class myClass{
+
+    static void Main(){
+        Console.WriteLine();
+        Console.WriteLine("===================================");
+
+        myClass.WelcomeMessage();
+        myClass.PromptUsername();
+        
+
+        Console.WriteLine("===================================");
+        Console.WriteLine();
+        
+    }
+
+    static void WelcomeMessage(){
+    // Void means there will be no parameters or a return type
+    Console.WriteLine("Welcome to My New Little Repository!");
+    }
+
+    static void PromptUsername(){
+        Console.WriteLine("Enter your username");
+        string userName = Console.ReadLine();
+    }
 }
