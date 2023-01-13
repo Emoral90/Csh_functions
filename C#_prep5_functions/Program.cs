2c45d36b958f3115d112755dd6c02c12fc71eef9
gasolina90 <moralerick90@gmail.com>
2023-01-12 17:59:03 -0700

    modified file

--- C#_prep5_functions/Program.cs
+++ C#_prep5_functions/Program.cs
@@ -4,14 +4,13 @@
 *
 *****************************************************************/
 
-// First 3 functions will be static
-// Or able to be called without creating a new instance of an object
 
 
 
 class myClass{
 
-    static void Main(){
+        // Void means there will be no parameters or a return type
+        static void Main(){
         Console.WriteLine();
         Console.WriteLine("===================================");
 
@@ -23,14 +22,31 @@ class myClass{
         Console.WriteLine();
         
     }
-
+    // First 3 functions will be static
+    // Or able to be called without creating a new instance of an object
     static void WelcomeMessage(){
-    // Void means there will be no parameters or a return type
     Console.WriteLine("Welcome to My New Little Repository!");
     }
 
     static void PromptUsername(){
         Console.WriteLine("Enter your username");
         string userName = Console.ReadLine();
+        Console.WriteLine($"Hello {userName}");
+    }
+
+    static void PromptUserNum(){
+        Console.WriteLine("Enter your ID number");
+        string UserNum = Console.ReadLine();
+        Console.WriteLine($"ID number; {UserNum}");
+    }
+
+    // Last 2 functions will not be static
+    // Or will have to be called by creating a new instance of the myClass object
+    int SquareNum(){
+        
+    }
+
+    string DisplayRes(){
+
     }
 }
